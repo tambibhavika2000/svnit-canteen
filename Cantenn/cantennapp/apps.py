a02e3c5c0840d9d466dd6bd894805a8bc474cb92
Jinal-P5 <talksjini@gmail.com>
2021-04-11 23:34:02 +0530

    added models in food app

--- Cantenn/cantennapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CantennappConfig(AppConfig):
-    name = 'cantennapp'
---
+++ Cantenn/food/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import MenuItem, OrderModel
+
+admin.site.register(MenuItem)
+admin.site.register(OrderModel)
+# Register your models here.
